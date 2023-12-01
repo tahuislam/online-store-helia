070057dee8b54f272ccc1a680ea9b125e65ba765
tahuislam <knocktahu@gmail.com>
2023-12-01 17:11:49 +0600

    img src 8

--- src/enum/products.js
+++ src/enum/products.js
@@ -2,7 +2,7 @@ const products = [
     {
         id:1,
         title:"Odere Chair II",
-        img:"/public/assets/productImages/p1.jpg",
+        img:"./assets/productImages/p1.jpg",
         price: 2344
     }, {
         id:2,
@@ -12,42 +12,42 @@ const products = [
     }, {
         id:3,
         title:"Pera G Otailava",
-        img:"./src/assets/productImages/p3.jpg",
+        img:"./assets/productImages/p3.jpg",
         price: 4563
     }, {
         id:4,
         title:"Guerot Melas",
-        img:"./src/assets/productImages/p4.jpg",
+        img:"./assets/productImages/p4.jpg",
         price: 5876
     }, {
         id:5,
         title:"Azar Velato",
-        img:"./src/assets/productImages/p5.jpg",
+        img:"./assets/productImages/p5.jpg",
         price: 5674
     }, {
         id:6,
         title:"Rela Veta Ni",
-        img:"./src/assets/productImages/p6.jpg",
+        img:"./assets/productImages/p6.jpg",
         price: 7775
     }, {
         id:7,
         title:"Igelera Moiste Era",
-        img:"./src/assets/productImages/p7.jpg",
+        img:"./assets/productImages/p7.jpg",
         price: 7775
     }, {
         id:8,
         title:"Pastru Ga Lish",
-        img:"./src/assets/productImages/p8.jpg",
+        img:"./assets/productImages/p8.jpg",
         price: 7775
     }, {
         id:9,
         title:"Ata Tukiye",
-        img:"./src/assets/productImages/p9.jpg",
+        img:"./assets/productImages/p9.jpg",
         price: 7775
     }, {
         id:10,
         title:"Otoman Eta",
-        img:"./src/assets/productImages/p10.jpg",
+        img:"./assets/productImages/p10.jpg",
         price: 7775
     }
 ];
